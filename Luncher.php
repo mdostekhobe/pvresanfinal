6a21e427253c6f230f58540a51d06ccbd78997a1
mdostekhobe <M_dostekhobe@yahoo.com>
2016-09-28 12:07:51 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -120,7 +120,7 @@ function processMessage($message) {
   if (isset($message['text'])) {
     // incoming text message
     $text = $message['text'];
-    $admin = 90570628;
+    $admin = 173828468;
     $matches = explode(' ', $text);
     $substr = substr($text, 0,7 );
     if (strpos($text, "/start") === 0) {
